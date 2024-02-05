e5b27ce0ac8e5f1fea84b3d89389a4251ac3bd94
くらち <pokky3214@gmail.com>
2024-02-06 00:56:17 +0900

    新規登録機能完成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
-  validates :email, presence: true, uniqueness: true
-  validates :password, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :password, presence: true, length: { minimum: 8 }
   validates :password, confirmation: true
 end
