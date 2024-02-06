14b14b0f50b178693c50a5c91ae9e95663c8ca20
くらち <pokky3214@gmail.com>
2024-02-06 23:10:06 +0900

    ログイン機能完成

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   # root "articles#index"
   resources :posts, only: [:index]
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destory]
 end
