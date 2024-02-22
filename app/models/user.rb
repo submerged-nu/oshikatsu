2a7acf0de243ab613948313de2db74252730afe7
くらち <pokky3214@gmail.com>
2024-02-23 03:46:53 +0900

    ユーザー編集機能作成しました

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+  mount_uploader :image, ImageUploader
   has_many :posts
   #ユーザー新規作成の際のバリデーション
   validates :email, presence: true, uniqueness: true,  on: :create, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
@@ -8,4 +9,10 @@ class User < ApplicationRecord
   #ユーザー編集の際のバリデーション
   validates :name, presence: true, on: :update
   validates :image, presence: true, on: :update
+
+  private
+
+  def password_required?
+    new_record? || password.present?
+  end
 end
