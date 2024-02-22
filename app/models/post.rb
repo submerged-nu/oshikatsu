eb7d74f0fc76f68a203d97ef177531e2059e5459
くらち <pokky3214@gmail.com>
2024-02-23 04:26:49 +0900

    投稿にタグをつける機能を作成しました

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,8 @@ class Post < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :user
   belongs_to :character
+  has_many :post_tags, dependent: :destroy
+  has_many :tags, through: :post_tags
   validates :image, presence: true
   validates :name, presence: true
   validates :body, presence: true
