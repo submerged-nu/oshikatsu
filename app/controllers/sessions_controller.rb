3c197ed3dfa8a23ff9622567eda261f32a5e092a
くらち <pokky3214@gmail.com>
2024-03-03 23:49:30 +0900

    ログアウトリンク設置

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session.delete(:user_id)
-    redirect_to new_session_path
+    redirect_to root_path
     flash[:success] = 'ログアウトしました'
   end
 end
