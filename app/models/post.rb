9bbc42f52946f7d06425029958a4b059741799c0
くらち <pokky3214@gmail.com>
2024-03-19 00:26:40 +0900

    jpg,pngのみを受け付けるようにjsを追加しました

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,9 +6,9 @@ class Post < ApplicationRecord
   has_many :tags, through: :post_tags
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
-  validates :image, presence: true
-  validates :name, presence: true
-  validates :body, presence: true
+  validates :image, presence: true, format: { with: /\A.*\.(png|jpg|jpeg)\z/i, message: 'jpg,png形式の画像を読み込ませてください' }
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :body, presence: true, length: { maximum: 500 }
 
   def self.ransackable_attributes(auth_object = nil)
     ["name"]
