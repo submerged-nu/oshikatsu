3d965f64ea0da7af5d053c9ce457304c853fabeb
くらち <pokky3214@gmail.com>
2024-03-26 21:10:58 +0900

    jsをいじる前

---
+++ spec/system/after_login_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper.rb'
+
+RSpec.describe 'ログイン後', type: :system do
+  describe '検索' do
+    it '検索ができること' do
+    end
+
+    it '検索がヒットしないこと' do
+    end
+  end
+
+  describe 'ログアウト' do
+    it 'ログアウトできること' do
+    end
+  end
+
+  describe 'コメント' do
+    it 'コメントができること'
+    end
+
+    it '200文字を超えるとコメントに失敗すること' do
+    end
+  end
+
+  describe 'ユーザー編集' do
+    it 'ユーザー編集ができること'
+    end
+  end
+end
+
+ランキング
