3d0c1ecde4f6e2230ddec5a10cff21c71eb2309c
くらち <pokky3214@gmail.com>
2024-03-28 06:14:59 +0900

    8行目を消した

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,6 +2,6 @@
 //= require bootstrap-sprockets
 //= require turbo
 //= require croppie
-
+//= require new_post_croppie_setup
+//= require user_edit_croppie_setup
 //= require close_modal
-//= require hide_modal_on_page_transition
