bf5002b15b3ed0c75a113e91cbc738298fff6379
くらち <pokky3214@gmail.com>
2024-04-09 16:06:04 +0900

    正しく動いた

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,13 @@
 class LikesController < ApplicationController
   include ActionView::RecordIdentifier
   before_action :require_login
-  before_action :set_liked_post_ids
 
   def create
     @post = Post.find(params[:post_id])
     @like = @post.likes.build(user: current_user)
 
     if @like.save
+      @liked_post_ids = current_user.likes.select(:post_id).map(&:post_id).to_set
       respond_to(&:turbo_stream)
     else
       flash.now[:danger] = 'エラーが発生しました'
@@ -19,12 +19,8 @@ class LikesController < ApplicationController
     @like = current_user.likes.find_by(post_id: params[:id])
     @post = @like.post
     @like.destroy
+    @liked_post_ids = current_user.likes.select(:post_id).map(&:post_id).to_set
     respond_to(&:turbo_stream)
   end
 
-  private
-
-  def set_liked_post_ids
-    @liked_post_ids = current_user.likes.select(:post_id).map(&:post_id).to_set if current_user
-  end
 end
