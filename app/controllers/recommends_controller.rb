046fe1e03ba10459f1d5723c50e148fe04d69ab7
くらち <pokky3214@gmail.com>
2024-04-18 12:10:27 +0900

    おすすめ機能できた

--- app/controllers/recommends_controller.rb
+++ app/controllers/recommends_controller.rb
@@ -2,19 +2,16 @@ class RecommendsController < ApplicationController
   before_action :require_login
 
   def index
-    liked_posts = current_user.likes.order(created_at: :desc).limit(20).map(&:post)
+    liked_posts = current_user.likes.order(created_at: :desc).limit(30).map(&:post)
     tag_counts = liked_posts.each_with_object(Hash.new(0)) do |post, counts|
       post.tags.each { |tag| counts[tag] += 1 }
     end
-    top_tags = tag_counts.sort_by { |_tag, count| -count }.map(&:first).take(3)
+    top_tags = tag_counts.sort_by { |_tag, count| -count }.map(&:first).take(10)
     @recommends = Post.joins(:tags)
                     .where(tags: { id: top_tags })
-                    .where.not(id: liked_posts.id)
+                    .where.not(id: liked_posts.map(&:id))
                     .distinct
                     .order('RANDOM()')
                     .page(params[:page])
   end
-
-  private
-
 end
