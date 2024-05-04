9a50641bcc1d3de432175423dd120efae03f8a96
くらち <pokky3214@gmail.com>
2024-05-04 21:35:41 +0900

    saveメソッドを入れただけで解決した

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -1,5 +1,11 @@
 class OauthsController < ApplicationController
 
+#完全な新規登録ができること
+#新規登録の際、アイコンとユーザー名が設定されること
+#すでにメールアドレスが登録されていて、それに対してgoogleでログインができること
+#うまくいかなかった場合、エラーを出すこと
+#特有のメソッドを理解して、そこからメールアドレスだけを抜き出してユーザー登録をする
+
   def oauth
     login_at(params[:provider])
   end
@@ -12,6 +18,7 @@ class OauthsController < ApplicationController
     else
       begin
         @user = create_from(provider)
+        @user.save
         reset_session
         auto_login(@user)
         flash[:notice] = 'グーグルアカウントで新規登録しました'
