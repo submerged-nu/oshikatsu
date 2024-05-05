3b435e7ca01ca2d06aa48620a523d2fe4b0464ee
くらち <pokky3214@gmail.com>
2024-05-06 00:27:11 +0900

    フロントはできた

--- app/assets/javascripts/tag.js
+++ app/assets/javascripts/tag.js
@@ -1,17 +1,15 @@
 $(document).ready(function() {
   $('#tags').on('cocoon:after-insert', function(e, insertedItem) {
-    let tagCount = $(".nested-fields").length;
-    let tagLabel = "タグ" + tagCount; // 例: "タグ1"
+    let tagCount = $(".nested-fields").length + 1;
+    let tagLabel = "タグ" + tagCount;
     insertedItem.find("label").first().text(tagLabel);
 
-    // タグが5個に達したら追加ボタンを非表示
     if (tagCount >= 5) {
       $("#add-tag-link").hide();
     }
   });
 
   $('#tags').on('cocoon:after-remove', function(e, removedItem) {
-    // タグが削除されたら、追加ボタンを再表示（もし隠れていれば）
     if ($(".nested-fields").length < 5) {
       $("#add-tag-link").show();
     }
